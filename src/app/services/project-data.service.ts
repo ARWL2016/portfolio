a86541fc1fc27a8e0b24ed9e59a6526647e51072
Alistair Willis <alistairrwillis@gmail.com>
2017-07-20 21:52:09 +0900

    add project data

--- src/app/services/project-data.service.ts
+++ src/app/services/project-data.service.ts
@@ -3,17 +3,42 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ProjectDataService {
 
-  constructor() { }
-
   projectData = [
     {
-      name: 'Votogo', 
-      short_description: 'Votogo is a free voting app that allows users to create, share and view the results of surveys. It is built with Angular, Node, Express and MongoDB.',
-      image_url: 'assets/votogo-sm.png', 
+      name: 'Votogo',
+      short_description: `
+        Votogo is a full stack CRUD application that allows users to create,
+        share and view the results of surveys. It is built with Angular, Node, Express and MongoDB
+        and includes an authentication module using BCrypt.js for password encryption and JSON Web Tokens
+        for secure sessions
+          `,
+      image_url: 'assets/votogo-sm.png',
+      image_order: 0
+    },
+    {
+      name: 'Stock Tracker',
+      short_description: `
+        Stock Tracker is an AngularJS application which charts time series data from the FTSE 100
+        using the Quandl API. Data is cached once a day on a MySQL database, ensuring rapid load times.
+        The back end is written in Node and Express.
+        `,
+      image_url: 'assets/stocko.png',
+      image_order: 2
+    },
+    {
+      name: 'Calculator',
+      short_description: `
+        This calculator was originally written in jQuery then refactored to native Javascript to reduce
+        unnecessary dependencies. Webpack is used as a build tool to transpile ES6 and process the Sass stylesheets.
+        The project is tested with Mocha and JSDom.
+        `,
+      image_url: 'assets/calculator.png',
       image_order: 0
     }
-  ]; 
+  ];
 
-  
+  constructor() { }
+
+
 
 }
